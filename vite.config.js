7f6c1da23ab44f28922cca9030ddb8c965465b0c
ZulqurnainCheema <zulqurnaincheema45@gmail.com>
2024-08-16 23:42:57 +0500

    I did it knowingly

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/todolist/",
+  base: '/Todolist-app/',
   plugins: [react()],
+
   
 });
